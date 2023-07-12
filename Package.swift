7737f957ad856b1ebd726c82ade62fbeefbba01b
Giorgi Vashakidze <gvashakidze@kaltura.com>
2023-07-12 12:11:37 +0400

    ...

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "CalendarView-iOS",
-            dependencies: ["Sources"]
+            path: "Calendar"
         ),
         .testTarget(
             name: "CalendarView-iOSTests",
