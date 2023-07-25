7a79299e3c78ceb25afd16feeec0b2ae5d98b627
George Vashakidze <gvashakidze6@gmail.com>
2023-07-25 18:23:09 +0400

    updated target specs

--- Package.swift
+++ Package.swift
@@ -10,13 +10,13 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "CalendarView",
-            targets: ["CalendarView"]),
+            targets: ["Calendar"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "CalendarView",
+            name: "Calendar",
             path: "Calendar/Source"
         ),
         .testTarget(
